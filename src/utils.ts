4f09d9fe3b0d99d3be971353fc5eb6ec6b26b22a
Gabriele Cannata <gcannata@gmail.com>
2023-07-10 22:06:23 +0200

    restored cartesian product

--- src/utils.ts
+++ src/utils.ts
@@ -14,25 +14,25 @@ export function* cartesianProductLazy(arr:any[][], i = 0):Generator<any[], void,
 }
 
 export function* filteredCartesianProduct(arr:any[], predicate:(...vars:any[])=>boolean):Generator<any[], void, unknown> {
-    let checked = new Set();
-    let unchecked = new Set();
+    // let checked = new Set();
+    // let unchecked = new Set();
     for (let product of cartesianProductLazy(arr)) {
-        let sortedProduct = product.slice().sort();
-        let key = JSON.stringify(sortedProduct);
-        // if (predicate(...product)) {
-        //     yield product;
-        // }
-        let satisfied = false
-        if (!checked.has(key) && !unchecked.has(key)) {
-            satisfied = predicate(...product);
-            (satisfied ? checked : unchecked).add(key)
-            
-        } else {
-            satisfied = checked.has(key)
-        }
-        if (satisfied) {
+        // let sortedProduct = product.slice().sort();
+        // let key = JSON.stringify(sortedProduct);
+        if (predicate(...product)) {
             yield product;
         }
+        // let satisfied = false
+        // if (!checked.has(key) && !unchecked.has(key)) {
+        //     satisfied = predicate(...product);
+        //     (satisfied ? checked : unchecked).add(key)
+            
+        // } else {
+        //     satisfied = checked.has(key)
+        // }
+        // if (satisfied) {
+        //     yield product;
+        // }
     }
 }
 
